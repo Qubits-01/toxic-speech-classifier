cad12516ecafa224cef99cdcf754f0fdea404ccf
Marc Peejay Viernes <mpvviernes@gmail.com>
2023-06-07 20:20:37 +0800

    Improve the documentaiton of how the AI model was built

--- backend/main.py
+++ backend/main.py
@@ -334,7 +334,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-# Sample endpoint.
+# Sample endpoint with a query.
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}

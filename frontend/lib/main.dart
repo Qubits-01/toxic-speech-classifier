e71a31c7d3138c748767c72d901905d91d175ad8
Marc Peejay Viernes <mpvviernes@gmail.com>
2023-06-07 16:39:41 +0800

    Backend: Implement a FastAPI endpoint to classify an input text

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
 
 import 'router/app_router.dart';
 import 'themes/app_theme.dart';
 
 void main() {
+  // Initialize the Appwrite client.
+  Client client = Client();
+  client
+      .setEndpoint('https://cloud.appwrite.io/v1')
+      .setProject('toxic_speech_classifier')
+      .setSelfSigned(status: true);
+  // For self signed certificates, only use for development.
+
   runApp(const ToxicSpeechClassifierApp());
 }
 
